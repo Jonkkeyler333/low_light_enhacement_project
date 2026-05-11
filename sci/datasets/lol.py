5cea7b9dca39785e72c057c1d2286213ae6c68c7
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2026-05-10 23:10:21 -0500

    feat(data) : add dataset defined class

---
+++ sci/datasets/lol.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import os
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+class LOLDataset(Dataset):
+    def __init__(self, root_dir: str, train:bool = True) -> None:
+        super().__init__()
+        self.root_dir = root_dir
+        self.train = train
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        self.low_images = []
+        self.high_images = []
+        if self.train:
+            self.low_dir = os.path.join(root_dir, 'train/low')
+            self.high_dir = os.path.join(root_dir, 'train/high')
+        else:
+            self.low_dir = os.path.join(root_dir, 'test/low')
+            self.high_dir = os.path.join(root_dir, 'test/high')    
+        for file in os.listdir(self.low_dir):
+            if file.endswith('.png'):
+                image_path = os.path.join(self.low_dir, file)
+                self.low_images.append(image_path)
+        for file in os.listdir(self.high_dir):
+            if file.endswith('.png'):
+                image_path = os.path.join(self.high_dir, file)
+                self.high_images.append(image_path)
+    
+    def _load_image_transform(self, image_path: str) -> torch.Tensor:
+        image = Image.open(image_path).convert('RGB')
+        img_norm = self.transform(image)
+        if torch.is_tensor(img_norm):
+            return img_norm
+        else:
+            raise TypeError(f"Expected a torch.Tensor, but got {type(img_norm)}")
+        
+    def __len__(self) -> int:
+        return len(self.low_images)
+    
+    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor, str, str]:
+        low_image_path = self.low_images[index]
+        high_image_path = self.high_images[index]
+        low_image = self._load_image_transform(low_image_path)
+        high_image = self._load_image_transform(high_image_path)
+        return low_image, high_image, low_image_path, high_image_path
